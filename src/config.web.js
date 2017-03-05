bf0eae57eb9babb81a0e63caf1d7290ee5dee46f
Markus Tacker <m@coderbyheart.com>
2017-03-05 23:59:14 +0100

    feat(project): initial commit

---
+++ src/config.web.js
@@ -0,0 +1,13 @@
+import pjson from '../package.json'
+
+export const config = {
+  environment: process.env.ENVIRONMENT || 'production',
+  version: pjson.version,
+  name: pjson.name,
+  appName: 'Let\'s Work Remote',
+  description: '🇩🇪 Deutschsprachiges Portal rund um die Arbeit in #Remote Teams.  🏝️ Ein Projekt von @kamilleblumm 🏵️ und @coderbyheart ✨⌨️.',
+  lang: 'de',
+  webHost: process.env.WEB_HOST || 'https://lets-work-remote.de'
+}
+
+process.stdout.write(JSON.stringify(config))
